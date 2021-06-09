fb4e75dd06778b1a40be47996103796097e2a493
humpty dumpty <spyker5@creationuq.com>
2021-06-09 14:12:27 +0300

    added rough bootstrap to home

--- src/components/AlbumArticle.jsx
+++ src/components/AlbumArticle.jsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import SpotifyPlaylist from './SpotifyPlaylist';
+//import SpotifyPlaylist from './SpotifyPlaylist';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/AlbumArticle.scss';
 
 function AlbumArticle(props) {
     return (
-        <div className="album-everything">
-            <p className="album-paragraph">
+        <div className="album-everything container">
+            <p className="album-paragraph row">
                 {
                     props.article
                 }
             </p>
-            <div className="cover-with-playlist">
-                <img className="album-cover" 
+            <div className="cover-with-playlist row">
+                <img className="album-cover col-sm-6" 
                     src={props.cover}
                     alt="album-cover"
                 />
 
-                <iframe className="spotify-playlist"
+                <iframe className="itunes-playlist col-sm-4"
+                    allow="encrypted-media *;" 
+                    frameBorder="0" 
+                    height="override in css" 
+                    sandbox="allow-top-navigation-by-user-activation" 
+                    src={props.playlistUrl} 
+                    title={props.title}
+                />
+            </div>
+
+        </div>
+    )
+}
+
+export default AlbumArticle;
+
+/*                 <iframe className="spotify-playlist"
                     src={props.playlistUrl} 
                     width="30%" 
                     height="no, not here, in the css" 
@@ -24,11 +41,6 @@ function AlbumArticle(props) {
                     allowtransparency="true" 
                     allow="encrypted-media"
                     title={props.title}
-                />
+                /> */
 
-            </div>
-        </div>
-    )
-}
 
-export default AlbumArticle;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import SpotifyPlaylist from '../components/SpotifyPlaylist';
+//import SpotifyPlaylist from '../components/SpotifyPlaylist';
 import AlbumArticle from '../components/AlbumArticle';
 import aceOfSpades from '../assets/images/album_aceofspades.jpg';
 import orgasmatron from '../assets/images/album_orgasmatron.jpg';
@@ -10,7 +10,7 @@ import '../scss/Home.scss';
 function Home() {
     return (
         <div className="home-container">
-            <AlbumArticle article={albumDescriptions.ACE_OF_SPADES}
+{/*             <AlbumArticle article={albumDescriptions.ACE_OF_SPADES}
                 cover={aceOfSpades}
                 playlistUrl="https://open.spotify.com/embed/album/3rxF05Aux0QTrN533Kjc91?theme=0"
                 title="ace-of-spades"
@@ -26,7 +26,32 @@ function Home() {
                 cover={sacrifice}
                 playlistUrl="https://open.spotify.com/embed/album/08ovGnoHDTOP6NqbtaFqW7?theme=0"
                 title="sacrifice"
+            /> */}
+
+            <AlbumArticle article={albumDescriptions.ACE_OF_SPADES}
+                cover={aceOfSpades}
+                playlistUrl="https://embed.music.apple.com/us/album/ace-of-spades-expanded-edition/1141599504"
+                title="ace-of-spades"
             />
+
+            <AlbumArticle article={albumDescriptions.ORGASMATRON}
+                cover={orgasmatron}
+                playlistUrl="https://embed.music.apple.com/us/album/orgasmatron-expanded-bonus-track-edition/1143706159"
+                title="orgasmatron"
+            />
+
+            <AlbumArticle article={albumDescriptions.SACRIFICE}
+                cover={sacrifice}
+                playlistUrl="https://embed.music.apple.com/us/album/sacrifice/1446663543"
+                title="sacrifice"
+            />
+         
+{/*             <AlbumArticle article={albumDescriptions.SACRIFICE}
+                cover={sacrifice}
+                playlistUrl="https://embed.music.apple.com/us/album/on-parole-expanded-and-remastered/1526305863" //DOESN T SHOW
+                title="on-parole"
+            /> */}
+         
         </div>
     )
 }
