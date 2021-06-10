686c478bae93dbf3f6bd7722b6c9d3d21af46056
humpty dumpty <spyker5@creationuq.com>
2021-06-10 12:31:43 +0300

    navbar, main container and bg are responsive

--- src/components/AlbumArticle.jsx
+++ src/components/AlbumArticle.jsx
@@ -6,27 +6,28 @@ import '../scss/AlbumArticle.scss';
 function AlbumArticle(props) {
     return (
         <div className="album-everything container">
-            <p className="album-paragraph row">
-                {
-                    props.article
-                }
-            </p>
-            <div className="cover-with-playlist row">
-                <img className="album-cover col-sm-6" 
-                    src={props.cover}
-                    alt="album-cover"
-                />
+            <div className="row">
+                <p className="album-paragraph col-sm-12" /* row" */>
+                    {
+                        props.article
+                    }
+                </p>
+                {/* <div className="cover-with-playlist row"> */}
+                    <img className="album-cover col-sm-6" 
+                        src={props.cover}
+                        alt="album-cover"
+                    />
 
-                <iframe className="itunes-playlist col-sm-4"
-                    allow="encrypted-media *;" 
-                    frameBorder="0" 
-                    height="override in css" 
-                    sandbox="allow-top-navigation-by-user-activation" 
-                    src={props.playlistUrl} 
-                    title={props.title}
-                />
-            </div>
-
+                    <iframe className="itunes-playlist col-sm-4"
+                        allow="encrypted-media *;" 
+                        frameBorder="0" 
+                        height="override in css" 
+                        sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
+                        src={props.playlistUrl} 
+                        title={props.title}
+                    />
+                {/* </div> */}
+            </div>        
         </div>
     )
 }
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import AlbumArticle from '../components/AlbumArticle';
 import aceOfSpades from '../assets/images/album_aceofspades.jpg';
 import orgasmatron from '../assets/images/album_orgasmatron.jpg';
+import album1916 from '../assets/images/1916.jpg';
+import theBest from '../assets/images/the_best_of_motorhead.jpg';
 import * as albumDescriptions from '../text/HomeText';
 import sacrifice from '../assets/images/album_sacrifice.jpg';
 import '../scss/Home.scss';
@@ -10,27 +12,15 @@ import '../scss/Home.scss';
 function Home() {
     return (
         <div className="home-container">
-{/*             <AlbumArticle article={albumDescriptions.ACE_OF_SPADES}
-                cover={aceOfSpades}
-                playlistUrl="https://open.spotify.com/embed/album/3rxF05Aux0QTrN533Kjc91?theme=0"
-                title="ace-of-spades"
-            />
-
-            <AlbumArticle article={albumDescriptions.ORGASMATRON}
-                cover={orgasmatron}
-                playlistUrl="https://open.spotify.com/embed/album/5QgYWE7hIuwFOFS20rE10o?theme=0"
-                title="orgasmatron"
-            />
-
             <AlbumArticle article={albumDescriptions.SACRIFICE}
-                cover={sacrifice}
-                playlistUrl="https://open.spotify.com/embed/album/08ovGnoHDTOP6NqbtaFqW7?theme=0"
-                title="sacrifice"
-            /> */}
+                cover={album1916}
+                playlistUrl="https://embed.music.apple.com/us/album/1916/192751993"
+                title="1916"
+            />  
 
             <AlbumArticle article={albumDescriptions.ACE_OF_SPADES}
                 cover={aceOfSpades}
-                playlistUrl="https://embed.music.apple.com/us/album/ace-of-spades-expanded-edition/1141599504"
+                playlistUrl="https://embed.music.apple.com/us/album/ace-of-spades/1141561107"
                 title="ace-of-spades"
             />
 
@@ -45,16 +35,13 @@ function Home() {
                 playlistUrl="https://embed.music.apple.com/us/album/sacrifice/1446663543"
                 title="sacrifice"
             />
-         
-{/*             <AlbumArticle article={albumDescriptions.SACRIFICE}
-                cover={sacrifice}
-                playlistUrl="https://embed.music.apple.com/us/album/on-parole-expanded-and-remastered/1526305863" //DOESN T SHOW
-                title="on-parole"
-            /> */}
-         
+            <AlbumArticle article={albumDescriptions.SACRIFICE}
+                cover={theBest}
+                playlistUrl="https://embed.music.apple.com/us/album/the-best-of-mot%C3%B6rhead/1143689217"
+                title="-the-best-of-motorhead"
+            />            
         </div>
     )
 }
 
 export default Home;
-
