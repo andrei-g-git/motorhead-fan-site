b6472636b956d9381a1a739915f6260bef556a19
humpty dumpty <spyker5@creationuq.com>
2021-06-19 11:47:24 +0300

    fixed nav-menu-mobile-animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,28 +19,13 @@ class Navbar extends Component {
         this.fangCount = [1, 2, 3, 4, 5, 6, 7, 8];
         this.bottomFangCount = [1, 2, 3, 4, 5, 6, 7, 8, 9];
     }
-
+  
     render() {
 
-        this.state.goingUp ? //I should probably just use a damn IF statement at this point
+        this.state.goingUp ? 
             this.secondClass = "going-up"
         :
             this.secondClass = "going-down"
-
-        this.props.menuVisible ? 
-            this.menuVisibility = " menu-visible" //mind the space
-        :
-            this.menuVisibility = ""
-
-        this.props.menuVisible ? 
-            this.navItemVisibility = "nav-item-visible"// NO SPACE
-        :
-            this.navItemVisibility= ""
-
-        this.props.menuVisible ? 
-            this.invisibilityClass = " "
-        :
-            this.invisibilityClass = " display-none"
 
         this.props.menuVisible ? 
             this.animationDirectionClass = " menu-down"
@@ -50,12 +35,11 @@ class Navbar extends Component {
         return (
             <div className={"nav-bar " + this.secondClass}>
 
-                <img className="hamburger-menu" //obv should be a component - or maybe not, I just need an onclick and the implementation is related to nav elements...
+                <img className="hamburger-menu" 
                     src={hamburger}
                     alt="hamburger"
                     onClick={() => {
                         this.props.handleMenuClick(!this.props.menuVisible);
-                        //this.switchNavItemOrder(this.props.menuVisible)
                     }}
                 />
 
@@ -66,39 +50,34 @@ class Navbar extends Component {
                     />
                 </a>
                 
-                {/* <div className={"drop-menu-bg" + this.invisibilityClass}> */}
-                <div className={"nav-items" + this.menuVisibility + this.animationDirectionClass}>
+                <div className={"nav-items menu-visible" + this.animationDirectionClass}>
 
                     <a className="nav-item"
-                        id={this.navItemVisibility + "_1"} //no spaces, ids can't have multiple, well, ids
                         href="/"
                     >
                         Home
                     </a>
 
                     <a className="nav-item"
-                        id={this.navItemVisibility + "_2"}
                         href="/albums"
                     >
                         Albums
                     </a>
                 
                     <a className="nav-item"
-                        id={this.navItemVisibility + "_3"}
                         href="/media"
                     >
                         Media
                     </a>
 
                     <a className="nav-item"
-                        id={this.navItemVisibility + "_4"}
                         href="/contact"
                     >
                         Contact
                     </a>
 
                     {/* BOTTOM FANGS FOR RETRACTABLE MENU */}
-                    <div className={"lower-fang-border-wrapper" + this.invisibilityClass}> {/* the imvisibility class may not work with the animated transition, may be instant */}
+                    <div className="lower-fang-border-wrapper" > 
                         <div className="nav-half-border"> {/* make component */}
                             {
                                 this.bottomFangCount.map((each, index) => 
@@ -113,7 +92,6 @@ class Navbar extends Component {
                     </div>
 
                 </div>
-                {/* </div> */}     
 
                 <div className="nav-border-wrapper">
                     <div className="nav-half-border"> {/* make component */}
@@ -141,7 +119,7 @@ class Navbar extends Component {
                     </div>
                 </div>  
 
-                {/* <div className={"test" + this.invisibilityClass} /> */}   {/* obscures too many nav elements, raising their z-index doesn't work */}                                                         
+                {/* <div className="obscure-nav-items" /> */} {/* doesn't work */}                                                       
             </div>
         )
     }
@@ -171,20 +149,6 @@ class Navbar extends Component {
         })
         console.log("going up     " + this.state.goingUp); //this isn't in real time, setState is async       
     }   
-    
-    //don't need anymore
-    switchNavItemOrder = (menuVisible) => { //is the menuVisible state ASYNC???
-        this.actualMenuVisibleBecauseTheStateHasntYetChanged = !menuVisible;
-        this.navItems = document.getElementsByClassName("nav-items")[0]; 
-        this.logo = document.getElementsByClassName("nav-logo")[0];
-        if(this.actualMenuVisibleBecauseTheStateHasntYetChanged){
-            this.navItems.insertBefore(this.logo, this.navItems.firstChild);
-        } else {
-            this.navItems.insertBefore(this.logo, this.navItems.children[3]);
-        }
-        console.log(this.navItems.firstChild.className)
-        console.log(this.navItems.children[1].className + "####")
-    }
 }
 
 const mapStateToProps = (state) => {
